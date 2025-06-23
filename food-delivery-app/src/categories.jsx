c051c20b6fec94a5877bd745e493ee84d24c340f
RupaliShelki <114135175+RupaliShelki@users.noreply.github.com>
2025-06-23 13:04:13 +0530

    categories

--- food-delivery-app/src/categories.jsx
+++ food-delivery-app/src/categories.jsx
@@ -2,53 +2,60 @@ import { RiGalleryView2 } from "react-icons/ri";
 import { MdOutlineFreeBreakfast } from "react-icons/md";
 import { LuSoup } from "react-icons/lu";
 import { CiBowlNoodles } from "react-icons/ci";
+import { GiOpenedFoodCan } from "react-icons/gi";
+import { MdOutlineLunchDining } from "react-icons/md";
+import { LuPizza } from "react-icons/lu";
 
 
 
-export const catagoriees=[
+
+const catagoriees=[
     {
         id:1,
         name:"all",
-        image: <RiGalleryView2 />
+        image: <RiGalleryView2  className="w-[60px] h-[60px] text-red-400"/>
 
     },
      {
         id:2,
         name:"breakfast",
-        image:<MdOutlineFreeBreakfast />
+        image:<MdOutlineFreeBreakfast className="w-[60px] h-[60px] text-red-400" />
 
 
     },
      {
         id:3,
         name:"soup",
-        image: <LuSoup />
+        image: <LuSoup className="w-[60px] h-[60px] text-red-400" />
 
 
     },
      {
-        id:1,
+        id:4,
         name:"pasta",
-        image: <CiBowlNoodles />
+        image: <CiBowlNoodles className="w-[60px] h-[60px] text-red-400" />
 
 
     },
      {
-        id:1,
-        name:"all",
-        image: <RiGalleryView2 />
+        id:5,
+        name:"main cource",
+        image: <GiOpenedFoodCan className="w-[60px] h-[60px] text-red-400" />
 
     },
      {
-        id:1,
-        name:"all",
-        image: <RiGalleryView2 />
+        id:6,
+        name:"pizza",
+        image: <LuPizza className="w-[60px] h-[60px] text-red-400" />
 
     },
      {
-        id:1,
-        name:"all",
-        image: <RiGalleryView2 />
+        id:7,
+        name:"burger",
+        image:<MdOutlineLunchDining className="w-[60px] h-[60px] text-red-400" />
+
 
     }
 ]
+
+export  default catagoriees
--- food-delivery-app/src/pages/home.jsx
+++ food-delivery-app/src/pages/home.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Nav from "../components/nav" 
+import React from "react";
+import Nav from "../components/nav";
+import catagoriees from "../categories";
 function home() {
   return (
-   <>
-   <div className='w-full min-h-screen bg-amber-100' >
-    <Nav></Nav>
-   </div>
-   
-   </>
-  )
+    <>
+      <div className="w-full min-h-screen bg-amber-100">
+        <Nav />
+        <div className="flex flex-wrap justify-center items-center gap-6 w-[100%]">
+          {catagoriees.map((item) => (
+            <div key={item.id} className="capitalize w-[138px] h-[138px] flex bg-slate-100 justify-center items-center flex-col p-[2%] shadow-md rounded-md text-[20px] font-semibold text-gray-600 hover:bg-amber-200 cursor-pointer transition-all duration-200">
+               <div>{item.image}</div>
+              <div>{item.name}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default home
+export default home;
