62c8940910c43262798a543a38b5b7f7d7745d1c
RupaliShelki <114135175+RupaliShelki@users.noreply.github.com>
2025-06-24 19:39:26 +0530

    context

--- food-delivery-app/src/contex/userContex.jsx
+++ food-delivery-app/src/contex/userContex.jsx
@@ -1,11 +1,15 @@
 import React, { createContext } from 'react'
-
-
 export const dataContex=createContext()
+
 function userContex({children}) {
+    let data= {
+
+    }
   return (
     <div>
+        <dataContex.Provider value={data}>
     {children}
+    </dataContex.Provider>
     </div>
   )
 }
