3c5df736cf6ef8d4c4552fc25f23ea0c0cfeb07d
RupaliShelki <114135175+RupaliShelki@users.noreply.github.com>
2025-07-03 19:43:06 +0530

    redux store created

--- food-delivery-app/src/components/card.jsx
+++ food-delivery-app/src/components/card.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import image1 from "../assets/image1.avif";
 import { LuVegan } from "react-icons/lu";
 import { GiChickenOven } from "react-icons/gi";
+import { useDispatch } from "react-redux";
+import { AddItem } from "../redux/cartSlice";
+
+
 
 function card({ name, image, id, price, type }) {
+
+  let dispatch = useDispatch();
   return (
     <div className="w-[280px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 hover:border-2 border-r-amber-700">
       <div className="w-[100%] h-[60%] overflow-hidden object-cover">
@@ -18,12 +24,15 @@ function card({ name, image, id, price, type }) {
           ) : (
             <GiChickenOven className="text-green-600" />
           )}
-          <span text-lg font-bold>
+          <span  className="text-lg font-bold ">
             {type}
           </span>
         </div>
       </div>
-      <button className="w-full p-3 bg-red-400  text-white rounded-lg hover:bg-amber-600 transition-all shadow-lg cursor-pointer ">
+      <button
+        className="w-full p-3 bg-red-400  text-white rounded-lg hover:bg-amber-600 transition-all shadow-lg cursor-pointer "
+        onClick={() => dispatch(AddItem(id))}
+      >
         Add to Dish
       </button>
     </div>
