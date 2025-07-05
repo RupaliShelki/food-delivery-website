a39008df016672915f0f0d1e69328f32a315a6a8
RupaliShelki <114135175+RupaliShelki@users.noreply.github.com>
2025-07-05 21:14:13 +0530

    quantity increase

--- food-delivery-app/src/redux/cartSlice.js
+++ food-delivery-app/src/redux/cartSlice.js
@@ -5,7 +5,13 @@ name:"cart",
 initialState:[],
 reducers:{
     AddItem:(state, action)=>{
-        state.push(action.payload)
+       let existItem = state.find((item)=>item.id===action.payload.id)
+       if(existItem){
+        return state.map((item)=>(item.id===action.payload.id?{...item,qty:item.qty+1}:item))
+       }else{
+ state.push(action.payload)
+       }
+       
     },
     RemoveItem:(state,action)=>{
         return state.filter((item)=>item.id!==action.payload)
